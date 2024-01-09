7fa1b66bf72b44b4773f604c0bf6b04937bba7ce
olya37 <156088135+olya37@users.noreply.github.com>
2024-01-09 15:52:21 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,47 @@
+using Calc_LAB2_Dolganova;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+        calculator.ShowUsage();
+        string pattern = @"^#\d+$";
+        double operand = calculator.EnterOperand();
+
+        while (true)
+        {
+            string useraction = calculator.EnterAction();
+            if (Regex.IsMatch(useraction, pattern))
+            {
+                calculator.GoToAction(useraction);
+            }
+            else
+            {
+                switch (useraction)
+                {
+                    case "+":
+                        operand = calculator.EnterOperand();
+                        calculator.PlusAction(operand);
+                        break;
+                    case "-":
+                        operand = calculator.EnterOperand();
+                        calculator.MinusAction(operand);
+                        break;
+                    case "*":
+                        operand = calculator.EnterOperand();
+                        calculator.MultiplyAction(operand);
+                        break;
+                    case "/":
+                        operand = calculator.EnterOperand();
+                        calculator.DivisionAction(operand);
+                        break;
+                    case "q":
+                        calculator.ExitAction();
+                        break;
+                }
+            }
+        }   
+    }
+}
